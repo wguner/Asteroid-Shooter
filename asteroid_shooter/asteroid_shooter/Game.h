5607c2993104f4bae82495471a4acc3fa52a7017
Setenay <setenay.guner@wsu.edu>
2018-12-04 11:54:02 -0800

    Update Game.h
    added isCollide()

--- asteroid_shooter/asteroid_shooter/Game.h
+++ asteroid_shooter/asteroid_shooter/Game.h
@@ -20,7 +20,28 @@ public:
 	void startRound();
 	void endRound();
 	void think();
+
+	bool isCollide(Game *first, Game *second)
+	{
+		return (second->x - first->x)*(second->x - first->x) + (second->y - first ->y)*
+			(second ->y - first->y)<(first->radius + second->radius)*(first->radius + second->radius);
+	}
+	/*
+	circle.setPosition(x, y);
+	circle.setOrigin(rad, rad);
+
+	if (first == "Asteroid" && second == "Bullet")
+		if (isCollide(first, second)){
+			first->life = life - 1;
+			second->life = life - 1;
+
+	if (first == "Player" && second == "Asteroid")
+		if (isCollide(a, b)){
+			b->life = life - 1;
+	*/
+
 private:
+	float x, y, radius;
 	RenderWindow window;
 	Player ply;
 	vector<Bullet> blts;
